6711da21dcfa20f0d07ff16186767d79b482a01c
Theo <32236823+piggeywig2000@users.noreply.github.com>
2023-07-25 13:23:22 +0100

    Bump version

--- BMOnline.Common/BidirectionalUdp.cs
+++ BMOnline.Common/BidirectionalUdp.cs
@@ -13,7 +13,7 @@ namespace BMOnline.Common
 #if DEBUG
         public const byte PROTOCOL_VERSION = 0; //0 is used for test versions
 #else
-        public const byte PROTOCOL_VERSION = 3;
+        public const byte PROTOCOL_VERSION = 4;
 #endif
         private readonly UdpClient client;
 
